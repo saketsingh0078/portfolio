701fa58e128eaa6dc4fbd96ac173998022a634ec
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-10-23 20:44:45 +0530

    some changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,10 @@ export const Footer = () => {
   return (
     <div className="bg-slate-600 text-gray-400 flex flex-col items-center ">
       <div className="sm:w-[75%] w-[90%] text-center">
-        <h1>© 2024 Saket. All rights reserved.</h1>
+        <h1>© 2024 Saket.All rights reserved.</h1>
         <h1>
           About this website: built with React & Next.js (App Router & Server
-          Actions), TypeScript, Tailwind CSS, Framer Motion, React Email &
-          Resend hosting.
+          Actions), TypeScript, Tailwind CSS, React Email & Resend hosting.
         </h1>
       </div>
     </div>
